70bd05e70a34b26dad387fffbe0ba41b11394c39
Leo <leo@initiation.com>
2020-10-31 19:57:00 +0100

    strating to look to something

--- src/body/body.tsx
+++ src/body/body.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './body.scss'
 
 export const Body = () => {
@@ -8,7 +8,20 @@ export const Body = () => {
             <div className="body-content">
                 bonjour je suis TOTO
                 <Switch>
-                    <Route path="/input">
+                    <Route path="/"
+                        render={
+                            () => {
+                                return (
+                                    <Redirect to="/lists" />
+                                )
+                            }
+                        }
+                    />
+                    <Route path="/lists">
+                    </Route>
+                    <Route path="/join-list">
+                    </Route>
+                    <Route path="/connection">
                     </Route>
                 </Switch>
             </div>
